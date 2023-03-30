be7d9debc8f4e33fc37bba35da00011c4d88a542
NITIN NAUTIYAL <nitinnautiyal2016@gmail.com>
2023-03-30 22:14:29 +0530

    final

--- frontend/Components/Contact.tsx
+++ frontend/Components/Contact.tsx
@@ -13,7 +13,7 @@ const client = createTRPCProxyClient<appRouterType>({
   links: [
     loggerLink(),
     httpBatchLink({
-      url : "http://localhost:3000/trpc",
+      url : "https://nitins-folio-odwi.onrender.com/trpc",
     }),
   ]
 })
