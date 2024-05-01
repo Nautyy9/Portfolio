748c8dcde490a72d23ba3d027251db570a54c56d
NITIN NAUTIYAL <nitinnautiyal2016@gmail.com>
2024-05-02 02:03:40 +0530

    skill_responsive

--- frontend/Assets/svgComponents/index.ts
+++ frontend/Assets/svgComponents/index.ts
@@ -1,17 +1,17 @@
 import JsSvg from "./JsSvg";
-import CssSvg from "./CssSvg.tsx";
-import HtmlSvg from "./HtmlSvg.tsx";
-import NextSvg from "./NextSvg.tsx";
-import MongoSvg from "./MongoSvg.tsx";
-import NodeSvg from "./NodeSvg.tsx";
-import PrismaSvg from "./PrismaSvg.tsx";
-import ReactSvg from "./ReactSvg.tsx";
-import ReduxSvg from "./ReduxSvg.tsx";
-import SanitySvg from "./SanitySvg.tsx";
-import SocketSvg from "./SocketSvg.tsx";
-import ThreeSvg from "./ThreeSvg.tsx";
-import TrpcSvg from "./TrpcSvg.tsx";
-import TsSvg from "./TsSvg.tsx";
+import CssSvg from "./CssSvg";
+import HtmlSvg from "./HtmlSvg";
+import NextSvg from "./NextSvg";
+import MongoSvg from "./MongoSvg";
+import NodeSvg from "./NodeSvg";
+import PrismaSvg from "./PrismaSvg";
+import ReactSvg from "./ReactSvg";
+import ReduxSvg from "./ReduxSvg";
+import SanitySvg from "./SanitySvg";
+import SocketSvg from "./SocketSvg";
+import ThreeSvg from "./ThreeSvg";
+import TrpcSvg from "./TrpcSvg";
+import TsSvg from "./TsSvg";
 
 export {
   CssSvg,
